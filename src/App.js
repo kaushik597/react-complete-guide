47d18dec54acbeff0a8d42f4932f5cb7f1e4f301
kaushik yadav <kaushikyadavjallam005@gmail.com>
2021-02-22 20:56:51 +0530

    created a functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   Hi, Namasteeeeeeeeeeee manchigunnara nen manchigunna
-      // </div>
+      <div className="App">
+        Hi, Namasteeeeeeeeeeee manchigunnara nen manchigunna
+        <Person />
+      </div>
 
-      React.createElement('div',{className:'App'},React.createElement('h2',null,`Nnamste react.create react element use chesi rastunna`))
     );
   }
 }
