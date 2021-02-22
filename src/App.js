492a829c2073b1f1cb55021b7887a1919c1c8733
kaushik yadav <kaushikyadavjallam005@gmail.com>
2021-02-22 20:41:47 +0530

    this code tells how jsx is converted by react

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hi, Namasteeeeeeeeeeee manchigunnara nen manchigunna
-      </div>
+      // <div className="App">
+      //   Hi, Namasteeeeeeeeeeee manchigunnara nen manchigunna
+      // </div>
+
+      React.createElement('div',{className:'App'},React.createElement('h2',null,`Nnamste react.create react element use chesi rastunna`))
     );
   }
 }
