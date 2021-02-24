1c34baa6ae48af3e6d3aea46ab1801434dfb4404
kaushik yadav <kaushikyadavjallam005@gmail.com>
2021-02-24 22:54:15 +0530

    using lists for better coding and reusing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
+import person from './Person/Person';
 
 class App extends Component {
   state={
     persons:[
-      {name:"kaushik",age:24},
-      {name:"karthik",age:26},
-      {name:"Dev in making",age:1}
+      {id:1,name:"kaushik",age:24},
+      {id:2,name:"karthik",age:26},
+      {id:3,name:"Dev in making",age:1}
     ]
   }
   deleteHandler=(personIndex)=>{
@@ -18,14 +19,22 @@ class App extends Component {
     
   }
 
-  nameChangeHandler = (event) => {
+  nameChangeHandler = (event,id) => {
     // donnt  do this as we cannot mutate state directly ------------->>>this.state.persons[0].name="Jallam Kaushik Yadav"
+   
+   const personIndex= this.state.persons.findIndex((p)=>{
+     return p.id===id;
+   })
+
+   const person={...this.state.persons[personIndex]}
+
+   person.name=event.target.value;
+
+   const persons = [...this.state.persons];
+   persons[personIndex]=person
+
     this.setState({
-      persons:[
-        {name:event.target.value,age:24},
-        {name:"karthik",age:26},
-        {name:"Dev in making",age:1}
-      ]
+      persons: persons
     })
   }
 
@@ -54,10 +63,11 @@ class App extends Component {
         <div>
           {this.state.persons.map((person,index)=>{
             return <Person 
+            key={person.id}
             click={()=>{this.deleteHandler(index)}  }
-          
             name={person.name}
-            age={person.age}/>
+            age={person.age}
+            changed={(event)=>this.nameChangeHandler(event,person.id)}/>
           })}
          </div>
       )
