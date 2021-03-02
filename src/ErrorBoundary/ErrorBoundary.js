b33d019f698c410492a85deb51b67bd1b0c316ca
kaushik yadav <kaushikyadavjallam005@gmail.com>
2021-03-02 17:36:05 +0530

    Revert "error boundary comp"
    This reverts commit 118ab5910e3f538b0630ddd218b3457a59b94ac1.

--- src/ErrorBoundary/ErrorBoundary.js
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react';
+import React , {Component} from 'react'
 
-class ErrorBoundary extends Component {
-    state = {
-        hasError: false,
-        errorMessage: ''
+class ErrorBoundary  extends Component{
+
+    state={
+        hasError:false,
+        errorMsg:''
     }
 
-    componentDidCatch = (error, info) => {
-        this.setState({hasError: true, errorMessage: error});
+    componentDidCatch=(error,info)=>{
+        this.setState({hasError:true,errorMsg:error})
+
     }
 
-    render() {
-        if (this.state.hasError) {
-            return <h1>{this.state.errorMessage}</h1>;
-        } else {
-            return this.props.children;
+    render(){
+        if(this.state.hasError){
+            return<h1>{this.state.errorMsg}</h1>
+        }
+
+        else{
+            return this.props.children
         }
     }
 }
