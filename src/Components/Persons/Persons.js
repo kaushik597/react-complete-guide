64f723ed9b02803f389621db22e23e444b708cde
kaushik yadav <kaushikyadavjallam005@gmail.com>
2021-03-03 21:59:52 +0530

    life cycle hooks while updating i.e, for change in props

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React , {Component} from 'react';
 import Person  from './Person/Person'
-const persons=(props)=>{
+class Persons extends Component{
 
+  static getDerivedStateFromProps(props,state){
+    console.log("[persons.js] getDerivedStateFromProps");
+    return state;
+  }
 
+  shouldComponentUpdate(nextProps,nextState){
+    console.log("[persons.js]  shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps,prevState){
+    console.log("[personss.js] getSnapshotBeforeUpdate");
+    return null;
+  }
+
+  componentDidUpdate(){
+    console.log("[persons.js] commponentDidUpdate");
+  }
+  render(){
     console.log("persons  js rendering");
 
-    return props.persons.map((person, index) => {
+    return this.props.persons.map((person, index) => {
 
         return (
             <Person
-            click={() => props.click(index)}
+            click={() => this.props.click(index)}
             name={person.name}
             age={person.age}
             key={person.id}
-            changed={event => props.changed(event, person.id)}
+            changed={event => this.props.changed(event, person.id)}
           />
          
         );
       })
+
+  }
+
+
+    
 }
 
 
-export default persons;
+export default Persons;
